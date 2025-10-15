1507449f176220e761066c2e8c514e4402d3776f
Aarav Shukla <aarav.shukla105@gmail.com>
2025-10-15 23:45:14 +0530

    Added the about and home page

---
+++ app/about/page.tsx
@@ -0,0 +1,12 @@
+"use client";
+
+import PillNavbar from '@/components/header';
+import '../globals.css';
+
+export default function About(){
+    return(
+        <div>
+            <PillNavbar/>
+        </div>
+    )
+}
